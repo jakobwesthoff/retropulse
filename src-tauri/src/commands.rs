f19d798f9f1c07f945b985489ed237cce6e021e4
Jakob Westhoff <jakob@westhoffswelt.de>
2024-09-15 23:43:18 +0200

    Refactor codebase by logically splitting into modules

---
+++ src-tauri/src/commands.rs
@@ -0,0 +1,33 @@
+use crate::player::{Player, PlayerEvent};
+use std::sync::Mutex;
+use tauri::State;
+
+#[tauri::command]
+pub fn load_module(player: State<Mutex<Player>>, filepath: &str) {
+    player.lock().unwrap().load(filepath);
+}
+
+#[tauri::command]
+pub fn play_module(player: State<Mutex<Player>>) {
+    player.lock().unwrap().play();
+}
+
+#[tauri::command]
+pub fn pause_module(player: State<Mutex<Player>>) {
+    player.lock().unwrap().pause();
+}
+
+#[tauri::command]
+pub fn subscribe_to_player_events(
+    player: State<Mutex<Player>>,
+    channel: tauri::ipc::Channel<PlayerEvent>,
+) -> String {
+    eprintln!("Subscribing to player events");
+    dbg!(player.lock().unwrap().subscribe_to_events(channel))
+}
+
+#[tauri::command]
+pub fn unsubscribe_from_player_events(player: State<Mutex<Player>>, id: String) -> bool {
+    eprintln!("Unsubscribing from player events with {}", id);
+    dbg!(player.lock().unwrap().unsubscribe_from_events(id))
+}
